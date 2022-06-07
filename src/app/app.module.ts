58b934e16e78609943b530979960ff3014540c72
“[Gabriel <[gabrielurbani@gmail.com]>
2022-06-07 11:57:46 -0300

    html y css to angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { NavSocialComponent } from './components/nav-social/nav-social.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent,
+    NavSocialComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
