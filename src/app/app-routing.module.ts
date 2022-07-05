a29f534b053304a70b1fba8cb46c5f7b1424a9e9
“[Gabriel <[gabrielurbani@gmail.com]>
2022-07-05 09:41:25 -0300

    add teatro &redes components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { TeatroComponent } from './components/teatro/teatro.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home', component:HomeComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'teatro', component: TeatroComponent},
+  {path: 'redes', component: RedesComponent},
+  {path:'**', redirectTo: 'home',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,29 @@ import { AppComponent } from './app.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { NavSocialComponent } from './components/nav-social/nav-social.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { TeatroComponent } from './components/teatro/teatro.component';
+import { CardLinkComponent } from './components/card-link/card-link.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RedesComponent } from './components/redes/redes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LogoComponent,
     NavSocialComponent,
-    HomeComponent
+    HomeComponent,
+    ProjectsComponent,
+    NavbarComponent,
+    HeaderComponent,
+    SliderComponent,
+    TeatroComponent,
+    CardLinkComponent,
+    FooterComponent,
+    RedesComponent
   ],
   imports: [
     BrowserModule,
